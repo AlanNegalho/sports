94ca54199cb6b717852dc3a66578b34451078f99
AlanNegalho <alandevads@gmail.com>
2024-02-19 22:06:36 -0300

    views e urls time

--- times/urls.py
+++ times/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.times, name='times'),
+    path('', views.atletas, name='atletas'),
     path('create/', views.create, name='create'),
     path('update/<int:id>/', views.update, name='update'),
     path('delete/<int:id>/', views.delete, name='delete'),
+    # Times
+    path('times/', views.times, name='times'),
+    path('ceate_time/', views.create_time, name='create_time'),
+    path('update_time/<int:id>/',views.update_time, name='update_time'),
+    path('delete_time/<int:id>/', views.delete_time, name= 'delete_time'),
+    
 ]
--- times/views.py
+++ times/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect, render
-from times.forms import JogadoresForm
-from times.models import Jogadores
+from times.forms import JogadoresForm, TimesForm
+from times.models import Jogadores, Times
 
 # Create your views here.
-def times(request):
+def atletas(request):
     jogadores = Jogadores.objects.all()
     return render(request, 'index.html', {'jogadores': jogadores})
 
@@ -12,7 +12,7 @@ def create(request):
         form = JogadoresForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('times')
+            return redirect('atletas')
     else:
         form = JogadoresForm()
     return render(request, 'jogadores/create.html', {'form': form})
@@ -22,10 +22,39 @@ def update(request, id):
     form = JogadoresForm(request.POST or None, instance=jogador)
     if form.is_valid():
         form.save()
-        return redirect('times')
+        return redirect('atletas')
     return render(request, 'jogadores/update.html', {'form': form, 'jogador': jogador})
 
 def delete(request, id):
     jogador = Jogadores.objects.get(id=id)
     jogador.delete()
+    return redirect('atletas')
+
+# Times
+
+def times (request):
+    times = Times.objects.all()
+    return render(request, 'time/times.html', {'times': times})
+
+def create_time(request):
+    if request.method == 'POST':
+        form = TimesForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('times')
+    else:
+        form = TimesForm()
+    return render(request, 'time/create.html', {'form': form})
+
+def update_time(request, id):
+    times = Times.objects.get(id=id)
+    form = TimesForm(request.POST or None, instance=times)
+    if form.is_valid():
+        form.save()
+        return redirect('times')
+    return render(request, 'time/update.html ', {'form': form, 'times': times})
+
+def delete_time(request, id):
+    times = Times.objects.get(id = id)
+    times.delete()
     return redirect('times')
